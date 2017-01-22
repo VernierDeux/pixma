0c5ef4b1931411c484defd7e63f0746cc72a5263
VernierDeux <gm.chro@gmail.com>
2017-01-23 02:35:50 +0300

    Add files via upload

---
+++ MoveObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour {
+
+public GameObject moveobject;
+public Transform targetPosition;
+public float Speed;
+	
+	void Start () {
+	
+	//Debug.Log("DoStart= " + Time.time);	
+	}
+	
+	
+	void Update () {
+		if(Input.GetKey(KeyCode.Space)){
+		if(Vector3.Distance(moveobject.transform.position,targetPosition.position) < 0.5f)
+		{
+			Destroy(moveobject,2);
+		}
+		else
+		{
+	moveobject.transform.position = Vector3.MoveTowards(moveobject.transform.position,targetPosition.position,Time.deltaTime*Speed);
+	moveobject.transform.LookAt(targetPosition);
+	//Debug.Log("DoUpdate");
+	    }
+		}
+	}
+}
