53fb25a080d684584b09d51779961f559f908eda
VernierDeux <gm.chro@gmail.com>
2017-01-23 03:05:53 +0300

    Add files via upload

---
+++ MoveBlocks.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBlocks : MonoBehaviour {
+
+	public float Speed;
+	public Transform targetPosition;
+	void Start () {
+	Destroy(gameObject,10);
+	}
+	
+	
+	void Update () {
+	transform.Translate(Vector3.forward*Time.deltaTime*Speed);
+	}
+}
