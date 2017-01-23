5ed716eeff321ddfa026871a12d8b9b933037fab
VernierDeux <gm.chro@gmail.com>
2017-01-23 03:20:42 +0300

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public GameObject Car;
+	void Start () {
+	Car = GameObject.Find("Car");
+	}
+	
+	
+	void Update () {
+		if(Car != null && Vector3.Distance(Car.transform.position,transform.position) < 1f)
+		{
+			Destroy(Car);
+			Application.LoadLevel(1);
+		}
+		}
+	}
+
