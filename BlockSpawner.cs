071a4ab5bcf89b66146ef363d002f4e034e62c68
VernierDeux <gm.chro@gmail.com>
2017-02-01 23:33:47 +0300

    Add files via upload

---
+++ BlockSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour {
+public GameObject BlockPrefab;
+public float SpawnTime;
+	void Start () {
+		
+		InvokeRepeating("SpawnBlock",0,SpawnTime);
+	}
+	
+	
+	void SpawnBlock () {
+	GameObject block = Instantiate(BlockPrefab,transform.position,transform.rotation) as GameObject;	
+	}
+}
